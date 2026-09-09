7055fd35a6caf021581520aa65537c8491358faa
JaiminRaval <siliconvalley.jaimin@gmail.com>
2026-09-09 09:54:27 +0530

    Initial Commit

---
+++ Sorting-Charts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sorting-Charts
+//
+//  Created by Jaimin Raval on 09/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sorting-Charts/Sorting_ChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sorting_ChartsApp.swift
+//  Sorting-Charts
+//
+//  Created by Jaimin Raval on 09/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Sorting_ChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
